a6b8bf400557dd2e828e47d67acb53e9a6589baf
Neha.Deshmukh <neha.deshmukh@asurion.com>
2021-08-10 22:22:37 +0530

    added factorial program & Palindrom program

--- src/Factorial.java
+++ src/Factorial.java
@@ -1,5 +1,13 @@
 public class Factorial {
+    int number;
+    public static void findFactorial(int number){
+        int fact=1;
+        for(int i=1;i<=number;i++){
+            fact=fact*i;
+        }
+        System.out.println("Factorial of "+number+"= "+fact);
 
+    }
     public static void main(String args[]){
         String s=new String("Hello");
         String s2=new String("Hello");
@@ -10,5 +18,7 @@ public class Factorial {
         else{
             System.out.println("No");
         }
+        findFactorial(5);
+
     }
 }
--- src/PalindromeProgram.java
+++ src/PalindromeProgram.java
@@ -1,5 +1,20 @@
 public class PalindromeProgram {
     public static void main(String[] args){
+        int r,sum=0, temp;
+        int number=454;
+        temp=number;
+        while(number>0){
+            r=number%10;
+            sum=(sum*10)+r;
+            number=number/10;
+        }
 
+        if(temp==sum){
+            System.out.println(temp+" is Palindrome number");
+        }
+        else
+        {
+            System.out.println(temp+" is not Palindrome number");
+        }
     }
 }
